1c280da3d577703714bae757df9d20afd10c2bb7
Subhankarc79 <subhankar.chatterjee@cgi.com>
2017-09-14 10:08:11 -0600

    #4

--- gradle/tasks/gradle-assemble.sh
+++ gradle/tasks/gradle-assemble.sh
@@ -12,6 +12,6 @@ echo "*** In Build ***"
 
 ls -lrt build/libs
 
-mv build/libs/${APP_NAME}*.jar ../build/${JAR_NAME}-${version}.jar
+#mv build/libs/${APP_NAME}*.jar ../build/${JAR_NAME}-${version}.jar
 
-#mv build/libs/app*.jar ../build/${JAR_NAME}-${version}.jar
+mv build/libs/app*.jar ../build/${JAR_NAME}-${version}.jar
